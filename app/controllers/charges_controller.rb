6d5f822f0d16e3be30d95c23ddfdd3dd7cdfa7c9
Kyle Geske <stungeye@gmail.com>
2016-11-29 10:23:14 -0600

    Added the code provided by the stripe tutorial to charges#create.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,5 +3,18 @@ class ChargesController < ApplicationController
   end
 
   def create
+    @amount = 500 # $5 in cents
+
+    customer = Stripe::Customer.create(email:  params[:stripeEmail],
+                                       source: params[:stripeToken])
+
+    charge = Stripe::Charge.create(customer:    customer.id,
+                                   amount:      @amount,
+                                   description: 'Rails Stripe customer',
+                                   currency:    'cad')
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
 end

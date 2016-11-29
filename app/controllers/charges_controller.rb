dc1d3fa4aa8b61ad61d4e2a829828ea7b7fe7005
Kyle Geske <stungeye@gmail.com>
2016-11-29 11:01:28 -0600

    Slight whitespace reformat in charges#create. Also now using the reported Stripe:charge amount on the success page.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,10 +8,10 @@ class ChargesController < ApplicationController
     @customer = Stripe::Customer.create(email:  params[:stripeEmail],
                                        source: params[:stripeToken])
 
-    @charge = Stripe::Charge.create(customer:   @customer.id,
-                                   amount:      @amount,
-                                   description: 'Rails Stripe customer',
-                                   currency:    'cad')
+    @charge = Stripe::Charge.create(customer:    @customer.id,
+                                    amount:      @amount,
+                                    description: 'Rails Stripe customer',
+                                    currency:    'cad')
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path

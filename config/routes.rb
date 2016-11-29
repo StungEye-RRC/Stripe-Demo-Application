02cc9c819d2cd37074fbc349a6026a8b4bafc1f4
Kyle Geske <stungeye@gmail.com>
2016-11-29 10:26:53 -0600

    Added the charges routes for new and create.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'charges/new'
-
-  get 'charges/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :charges, only: [:new, :create]
 end
